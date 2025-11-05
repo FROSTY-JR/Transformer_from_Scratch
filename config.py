27dea52068c3ba3afd925929eba54c0deab33ff8
Frosty_j.r <yashasbl15@gmail.com>
2025-11-05 15:40:03 +0530

    Add configuration file with model hyperparameters

---
+++ config.py
@@ -0,0 +1,12 @@
+#the file will be updated along the project timeline or as and when there is anything to be added.
+
+# Model Hyperparameters
+D_MODEL = 512
+N_HEADS = 8
+N_LAYERS = 6
+MAX_LEN = 5000
+
+# Training Settings
+BATCH_SIZE = 32
+LEARNING_RATE = 0.0001
+EPOCHS = 10
